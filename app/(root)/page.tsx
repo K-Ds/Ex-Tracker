5d5426d07a073c37033386644d32d8dafd4c28d6
David Karenzi <kardav2019@gmail.com>
2023-10-19 12:24:24 +0300

    redirect

--- app/(root)/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useRouter } from 'next/router';
-
-export default async function Home() {
-  const router = useRouter();
-  router.push('/dashboard');
-  return <div></div>;
-}
--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig
